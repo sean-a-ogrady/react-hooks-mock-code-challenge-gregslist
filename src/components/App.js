b8de0eb502a6b7640bb68630a626115df92db6a3
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-10-13 11:44:16 -0400

    Load and display listings

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const url = "http://localhost:6001/listings/"
+
+  const [listings, setListings] = useState([]);
+  const [query, setQuery] = useState("");
+  const [newListing, setNewListing] = useState({description: "", image: "", location: ""});
+
+  useEffect(() => {
+    fetch(url)
+    .then(response => response.json())
+    .then(setListings);
+  }, [])
+
+  const filteredListings = listings;
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={filteredListings} />
     </div>
   );
 }
